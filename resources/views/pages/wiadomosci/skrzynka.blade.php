aa720613b38455b165623e842991f77cbdb5af97
msiemdaj <epsadpersonal@gmail.com>
2020-07-15 17:47:13 +0200

    fixed checkboxes

--- resources/views/pages/wiadomosci/skrzynka.blade.php
+++ resources/views/pages/wiadomosci/skrzynka.blade.php
@@ -11,26 +11,17 @@
     </div>
   @endif
 
-  {{-- fix checkboxes na zmianie --}}
-  {{-- checkbox nie dodaje current statusu --}}
-
-
-{{-- <ul class="tabs">
-   <li class="tab-link current" data-tab="received">Odebrane</li>
-   <li class="tab-link" data-tab="sent">Wysłane</li>
- </ul> --}}
-
   <div class="container">
     <ul class="nav nav-skrzynka" id="tabMenu" role="tablist">
   <li>
-    <a class="nav-link a-skrzynka active" data-toggle="pill" href="#odebrane" role="tab">Odebrane</a>
+    <a class="nav-link a-skrzynka active" data-toggle="pill" href="#odebrane" role="tab" data-tab="received">Odebrane</a>
   </li>
   <li>
-    <a class="nav-link a-skrzynka" data-toggle="pill" href="#wyslane" role="tab">Wysłane</a>
+    <a class="nav-link a-skrzynka" data-toggle="pill" href="#wyslane" role="tab" data-tab="sent">Wysłane</a>
   </li>
 </ul>
-<div class="tab-content" id="pills-tabContent">
-  <div class="tab-pane fade show active" id="odebrane" role="tabpanel" aria-labelledby="pills-home-tab">
+<div class="tab-content">
+  <div class="tab-pane fade show active" id="odebrane" role="tabpanel">
     <div id="tabreceived" class="tab-content current">
       <h1>Odebrane wiadomości</h1>
       @if (count($messages_received) == 0)
@@ -94,7 +85,7 @@
   	</div>
   </div>
 
-  <div class="tab-pane fade" id="wyslane" role="tabpanel" aria-labelledby="pills-profile-tab">
+  <div class="tab-pane fade" id="wyslane" role="tabpanel">
     <div id="tabsent" class="tab-content">
       <h1>Wysłane wiadomości</h1>
       @if (count($messages_sent) == 0)
@@ -139,115 +130,7 @@
     </div>
   </div>
 </div>
-
-
-  	 {{-- <ul class="tabs">
-  		<li class="tab-link current" data-tab="received">Odebrane</li>
-  		<li class="tab-link" data-tab="sent">Wysłane</li>
-  	</ul>
-
-  	<div id="tabreceived" class="tab-content current">
-      <h1>Odebrane wiadomości</h1>
-      @if (count($messages_received) == 0)
-        <div class="empty-mailbox">
-          <img class="" src="{{asset('img/brakmsg.png')}}">
-        </div>
-      @else
-
-      <form method="get" action="/wiadomosci/skrzynkaoptions">
-        <div class="msg-options">
-          <div class="zaznaczWszystko">
-            <input type="checkbox" name="checkAll" class="checkAll" id="checkReceived">
-            <label for='checkReceived'>Zaznacz wszystko</label>
-          </div>
-            <input type="image" src="{{asset('img/bin32.png')}}" name="deletesubmit" class="skrzynka-options-btn skrzynka-btn-del">
-            <input type="image" src="{{asset('img/openemail32.png')}}" name="setReaded" id="setReaded" class="skrzynka-options-btn">
-            <input type="image" src="{{asset('img/closedemail32.png')}}" name="setUnreaded" id="setUnreaded" class="skrzynka-options-btn">
-        </div>
-
-      <table class="table messages-table">
-        <tbody>
-      @foreach($messages_received as $wiadomosc)
-        @if($wiadomosc->deleted_by_receiver == false)
-        <tr class="row-link" data-href="/wiadomosci/{{ $wiadomosc->id }}"
-          @if($wiadomosc->viewed == false)
-            id="new-message"
-          @endif
-          >
-            <td class="t-checkbox"> <input type="checkbox" @if($wiadomosc->viewed == false) class="checkbox-group current unreaded-message" @else class="checkbox-group current readed-message" @endif value="{{$wiadomosc->id}}" name="checkedMessage[]"> </td>
-            <td class="t-name"> Od: {{ $wiadomosc->name }} </td>
-            <td class="t-title">
-              @if($wiadomosc->viewed == false)
-                <b>{{ $wiadomosc->title }}</b>
-              @else
-                {{ $wiadomosc->title }}
-              @endif
-            </td>
-            <td class="t-body">{{ $wiadomosc->message_body }}</td>
-            <td class="t-date">
-              <div class="name-group">
-                {{ date('d/m/Y', strtotime($wiadomosc->sent_at)) }}
-              </div>
-
-              <div class="btn-group">
-                @if($wiadomosc->viewed == true)
-                  <a href="{{url('/wiadomosci/'.$wiadomosc->id.'/changeviewedstatus')}}"><img src="{{asset('img/closedemail16.png')}}"></a>
-                @elseif($wiadomosc->viewed == false)
-                  <a href="{{url('/wiadomosci/'.$wiadomosc->id.'/changeviewedstatus')}}"><img src="{{asset('img/openemail16.png')}}"></a>
-                @endif
-                  <a href="{{url('/wiadomosci/'.$wiadomosc->id.'/delete')}}"><img src="{{asset('img/bin16.png')}}"></a>
-              </div>
-            </td>
-        </tr>
-        @endif
-      @endforeach
-      </form>
-      </tbody>
-    </table>
-    @endif
-  	</div>
-  	<div id="tabsent" class="tab-content">
-      <h1>Wysłane wiadomości</h1>
-      @if (count($messages_sent) == 0)
-        <div class="empty-mailbox">
-          <img class="" src="{{asset('img/brakmsg.png')}}">
-        </div>
-      @else
-      <form method="get" action="/wiadomosci/skrzynkaoptions">
-        <div class="msg-options">
-          <div class="zaznaczWszystko">
-            <input type="checkbox" name="checkAll" class="checkAll" id="checkSent">
-            <label for='checkSent'>Zaznacz wszystko</label>
-          </div>
-          <input type="image" src="{{asset('img/bin32.png')}}" name="deletesubmit" class="skrzynka-options-btn skrzynka-btn-del">
-        </div>
-
-      <table class="table messages-table">
-        <tbody>
-        @foreach($messages_sent as $wiadomosc)
-          @if($wiadomosc->deleted_by_sender == false)
-          <tr class="row-link" data-href="/wiadomosci/{{ $wiadomosc->id }}">
-          <td class="t-checkbox"> <input type="checkbox" class="checkbox-group" value="{{$wiadomosc->id}}" name="checkedMessage[]"> </td>
-          <td class="t-name"> Do: {{ $wiadomosc->name }} </td>
-          <td class="t-title"> {{ $wiadomosc->title }} </td>
-          <td class="t-body"> {{ $wiadomosc->message_body }} </td>
-          <td class="t-date">
-            <div class="name-group">
-              {{ date('d/m/Y', strtotime($wiadomosc->sent_at)) }}
-            </div>
-            <div class="btn-group">
-              <a href="{{url('/wiadomosci/'.$wiadomosc->id.'/delete')}}"><img src="{{asset('img/bin16.png')}}"></a>
-            </div>
-          </td>
-        </tr>
-        @endif
-        @endforeach
-      </form>
-      </tbody>
-      </table>
-    @endif
-  	</div> --}}
-  </div>
+</div>
 
     <script>
     //redirect to specific tab
@@ -259,8 +142,9 @@
 
   <script type="application/javascript">
     $(document).ready(function(){
-    $('ul.tabs li').click(function(){
+    $('.a-skrzynka').click(function(){
       var tab = $(this).attr('data-tab');
+
       $(".checkbox-group").prop("checked", false);
       $(".checkAll").prop("checked", false);
       $('#setReaded').hide();
@@ -273,7 +157,6 @@
       $(this).addClass('current');
         $("#tab"+tab).addClass('current');
         $('.tab-content.current').find('.checkbox-group').addClass('current');
-
       })
     })
     </script>
@@ -296,6 +179,7 @@
   </script>
 
   <script type="application/javascript">
+  // pokaz opcje na hoverze > 992px
   var mq = window.matchMedia( "(min-width: 992px)" );
   if (mq.matches) {
     $('.row-link').hover(function(){
@@ -309,6 +193,7 @@
   </script>
 
   <script type="application/javascript">
+  // zaznacz wszystko
   $(document).ready(function(){
     $('.checkAll').click(function(){
       var checked = this.checked;
